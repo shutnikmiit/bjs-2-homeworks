33a24905c53c5d94a23972e0337fb69a410e0c44
shutnikmiit <shutnikmiit@mail.ru>
2022-07-14 14:19:41 +0300

    result

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -2,30 +2,48 @@
 function getArrayParams(arr) {
   let min, max, sum, avg;
 
-  // Ваш код
+  min = Math.min(...arr);
+  max = Math.max(...arr);
+  sum = 0;
+  for (let number of arr) {
+    sum += number;
+  }
+
+  avg = +(sum / arr.length).toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (let number of arr) {
+    sum += number;
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = 0;
 
-  // Ваш кода
-  // for ...
-  
-  return max;
-}
+  for (let arr of arrOfArr) {
+    for (let arr of arrOfArr) {
+      const funcResult = worker(arr);
+      if (max < funcResult) {
+        max = funcResult;
+      }
+    }
 
-// Задание 3
-function worker2(arr) {
-  // Ваш код
+    return max;
+  }
+
+  // Задание 3
+  function worker2(arr) {
+    const max = Math.max(...arr);
+    const min = Math.min(...arr);
+    const difference = max - min;
+    return Math.abs(difference);
+  }
 }

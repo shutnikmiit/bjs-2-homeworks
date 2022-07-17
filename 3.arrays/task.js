28e3c4b190b2b7208237b04ce40877f93ed3b4fb
shutnikmiit <shutnikmiit@mail.ru>
2022-07-17 17:09:57 +0300

    compareArrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,19 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
+  let result = arr1.every((item, index) => {
+    if (arr1.length !== arr2.length) {
+      return false;
+    }
+    if (item === arr2[index]) {
+      return true;
+    } else {
+      return false;
+    }
+  });
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
+  let resultArr = arr.filter(item => item >= 0 && item % 3 === 0).map(item => item * 10);
 
   return resultArr; // array
 }

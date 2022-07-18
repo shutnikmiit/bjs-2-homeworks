e678071b0da8ea506c8a7b4904953d2332e2c870
shutnikmiit <shutnikmiit@mail.ru>
2022-07-18 18:01:28 +0300

    objects_proto

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,41 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...grades) {
+  if (this.marks === undefined) {
+    this.marks = [...grades];
+  } else {
+    for (let grade of grades) {
+      this.marks.push(grade);
+    }
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let summ = 0;
+  for (let mark of this.marks) {
+    summ += mark;
+  }
+  return summ / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}

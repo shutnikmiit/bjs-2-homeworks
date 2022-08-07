56a3d929c011067864839288dc6fccdd7c9b4da7
shutnikmiit <shutnikmiit@mail.ru>
2022-08-07 20:32:41 +0300

    decorators

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,56 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  function wrapper(...args) {
+    const hash = args.join(',');
+    const objectInCache = cache.find(item => item.hash === hash);
+    if (objectInCache) {
+      console.log("Из кэша: " + objectInCache.value);
+      return "Из кэша: " + objectInCache.value;
+    }
+    let result = func(...args);
+    cache.push({ hash, value: result });
+
+    if (cache.length > 5) {
+      cache.shift();
+    }
+
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+
+function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
+
+  return function wrapper(...args) {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => func(...args), delay);
+    setTimeout(() => timeoutId = null, delay);
+  }
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+
+
+function debounceDecorator2(func, delay) {
+  let timeoutId = null;
+
+  function wrapper(...args) {
+    wrapper.count++;
+
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => func(...args), delay);
+    setTimeout(() => timeoutId = null, delay);
+  }
+  wrapper.count = 0;
+
+  return wrapper;
 }
